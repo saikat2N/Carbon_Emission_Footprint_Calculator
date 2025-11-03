b10823a880240173b2979d56129cdf8e0bd3f5f1
saikat2N <saikatbose1235@gmail.com>
2025-11-04 04:03:32 +0530

    Added Indian Emission Factors and Values.

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
+Emission_Factors= :{
+  "India": {
+    #kgC02 is the unit for carbon emission
+    "Transportation": 0.14, #kgCO2/km
+    "Electricity":0.82, # kgCO2/KwH
+    "Diet": 1.25, #kgCO2 per meal
+    "Waste": 0.1 #kgCO2/kg
+  }
+}
+
 st.title("Carbon Emission Footprint Calculator")
